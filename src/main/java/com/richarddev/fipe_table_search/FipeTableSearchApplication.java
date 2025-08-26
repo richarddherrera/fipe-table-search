93565dfad9102c521339ce2e4ee51d80a0c086d8
Richard Herrera <yrichardz3003@gmail.com>
2025-08-26 16:51:55 -0300

    Projeto sincronizado

---
+++ src/main/java/com/richarddev/fipe_table_search/FipeTableSearchApplication.java
@@ -0,0 +1,20 @@
+package com.richarddev.fipe_table_search;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class FipeTableSearchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeTableSearchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
---
+++ src/test/java/com/richarddev/fipe_table_search/FipeTableSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richarddev.fipe_table_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeTableSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
